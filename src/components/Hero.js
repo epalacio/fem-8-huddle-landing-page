d6dc2aeb97e58f892d33d566f0273e5d7eecff7c
Esteban Palacio <estipalacio@gmail.com>
2021-03-09 09:30:01 -0500

    added and tested custom colors/fonts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
             </div>
             <div>
                 <div>
-                    <h2>Build The Community Your Fans Will Love</h2>
+                    <h2 className="">Build The Community Your Fans Will Love</h2>
                     <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
                 </div>
                 <img src={mockups} alt="Desktop and mobile mockups, no text"/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      grayBlue: 'hsl(208, 11%, 55%)',
+      paleCyan: 'hsl(193, 100%, 96%)',
+      pinkEsp: 'hsl(322, 100%, 66%)',
+      darkCyan: 'hsl(192, 100%, 9%)'
+    },
     extend: {},
   },
   variants: {
