18947771c3f07fb0ba8e1d891d783397388c351c
Esteban Palacio <estipalacio@gmail.com>
2021-03-10 20:07:56 -0500

    Added footer styling for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,42 +8,42 @@ import logo2 from '../img/logo2.svg';
 
 const Footer = () => {
     return (
-        <div className="text-white bg-darkCyan pt-20">
+        <div className="text-white bg-darkCyan pt-20 px-4 ">
             <div>
-                <img src={logo2} alt=""/>
-                <div>
-                    <img src={location} alt=""/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                <img className="mb-6" src={logo2} alt=""/>
+                 <div className="flex items-baseline mb-4">
+                    <img className="" src={location} alt=""/>
+                    <p className="pl-6 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                 </div>
-                <div>
+                 <div className="flex items-baseline mb-4">
                     <img src={phone} alt=""/>
-                    <p>+1-543-123-4567</p>
+                    <p className="pl-6">+1-543-123-4567</p>
                 </div>
-                <div>
+                 <div className="flex items-baseline mb-4">
                     <img src={email} alt=""/>
-                    <p>example@huddle.com</p>
+                    <p className="pl-6">example@huddle.com</p>
                 </div>
 
             </div>
-            <div>
-                <ul>
+            <div className="grid grid-cols-1 gap-6 my-8 text-sm">
+                <ul className="grid grid-cols-1 gap-6">
                     <li>About Us</li>
                     <li>What We Do</li>
                     <li>FAQ</li>
                 </ul>
-                <ul>
+                <ul className="grid grid-cols-1 gap-6">
                     <li>Career</li>
                     <li>Blog</li>
                     <li>Contact Us</li>
                 </ul>
             </div>
-            <div>
-                <ul>
-                    <li><i className="fab fa-facebook-f"></i></li>
-                    <li><i className="fab fa-twitter"></i></li>
-                    <li><i className="fab fa-instagram"></i></li>
+            <div className="text-center text-sm">
+                <ul className="grid grid-cols-3 text-center justify-items-center mb-4 mx-12">
+                    <li className="border-white border-2 p-2 rounded-full w-12 h-12 text-center"><i className="pt-2 fab fa-facebook-f"></i></li>
+                    <li className="border-white border-2 p-2 rounded-full w-12 h-12 text-center"><i className="pt-2 fab fa-twitter"></i></li>
+                    <li className="border-white border-2 p-2 rounded-full w-12 h-12 text-center"><i className="pt-2 fab fa-instagram"></i></li>
                 </ul>
-                <p>&copy; Copyright 2021 Huddle. All rights reserved.</p>
+                <p className="text-sm pb-8">&copy; Copyright 2021 Huddle. All rights reserved.</p>
             </div>
         </div>
     );
