78a61b51b351781db43cc0576486eba1c7b9ba84
Esteban Palacio <estipalacio@gmail.com>
2021-03-13 18:43:58 -0500

    Completed fix for main hero banner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,10 +13,10 @@ const Hero = () => {
                 <Link className="rounded-full bg-white px-8 py-4 text-xs shadow-md text-center" to='/'>Try It Free</Link>
             </div>
             <div className="my-20 px-5 text-center grid gap-8 sm:grid-cols-2">
-                <div>
+                <div className="flex flex-col justify-center text-left">
                     <h2 className="font-bold text-xl ">Build The Community Your Fans Will Love</h2>
                     <p className="text-sm py-6">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
-                    <Link className="text-white rounded-full bg-pinkEsp px-12 py-2 text-xs shadow-md" to='/'>Get Started For Free</Link>
+                    <Link className="self-start text-white rounded-full bg-pinkEsp px-12 py-2 text-xs shadow-md" to='/'>Get Started For Free</Link>
                 </div>
                 <img  className="max-h-50 m-auto" src={mockups} alt="Desktop and mobile mockups, no text"/>
             </div>
