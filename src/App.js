cc265d4db18e120cfe84e5336fecfb165811cae0
Esteban Palacio <estipalacio@gmail.com>
2021-03-15 16:26:12 -0400

    fixed unused variable

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import Features from './components/Features';
 import Community from './components/Community';
 import Footer from './components/Footer';
-import Content from './components/subComponents/Content';
 
 const App = () => (
 //Fragment is temporary before setting up router
